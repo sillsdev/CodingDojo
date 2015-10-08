7c236b57657c61859c877903b7a926af905e2f13
John Thomson <john_thomson@sil.org>
2015-10-07 20:48:20 -0500

    Main algorithm working; failing test for bad args

--- PeasantMultiplication/PeasantMultiplication/PeasantMul.cs
+++ PeasantMultiplication/PeasantMultiplication/PeasantMul.cs
@@ -6,10 +6,17 @@ namespace PeasantMultiplication
 		{
 			var result = 0;
 			while (l > 0) {
-				result += r;
+				if (IsOdd(l))
+					result += r;
+				r <<= 1;
 				l >>= 1;
 			}
 			return result;
+		}
+
+		private static bool IsOdd(int l)
+		{
+			return (l & 1) != 0;
 		}
 	}
 }
--- PeasantMultiplication/PeasantMultiplication/TestMultiplication.cs
+++ PeasantMultiplication/PeasantMultiplication/TestMultiplication.cs
@@ -14,10 +14,20 @@ namespace PeasantMultiplication
 		[TestCase(1, 2, Result=2)]
 		[TestCase(2, 1, Result=2)]
 		[TestCase(3, 2, Result=6)]
+		[TestCase(9,5, Result=45)]
+		[TestCase(12,22, Result=12*22)]
+		[TestCase(0,1, Result=0)]
+		[TestCase(3,0, Result=0)]
 		[Test]
 		public int Mul(int a, int b)
 		{
 			return PeasantMul.Mul(a, b);
+		}
+
+		[Test]
+		public void NegativeThrows()
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(()=>PeasantMul.Mul(-1, 1));
 		}
     }
 }
