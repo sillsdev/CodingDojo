e4bc25dcbac3d76cbf7fd34519722102d0bfbc74
John Thomson <john_thomson@sil.org>
2015-10-07 20:03:09 -0500

    Initial commit - first failing test

---
+++ PeasantMultiplication/PeasantMultiplication/PeasantMul.cs
@@ -0,0 +1,10 @@
+namespace PeasantMultiplication
+{
+	public class PeasantMul
+	{
+		public static int Mul(int i, int i1)
+		{
+			return 0;
+		}
+	}
+}
---
+++ PeasantMultiplication/PeasantMultiplication/TestMultiplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PeasantMultiplication
+{
+	[TestFixture]
+    public class TestMultiplication
+    {
+		[Test]
+		public void Mul1Times1Is1()
+		{
+			Assert.That(PeasantMul.Mul(1,1), Is.EqualTo(1));
+		}
+    }
+}
